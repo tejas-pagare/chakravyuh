1bdcf720175b022cb91130813cbc40a1be22db7a
Tejas pagare <tejaspagare1625@gmail.com>
2024-11-06 23:31:50 +0530

    new

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { motion } from "framer-motion";
+import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
+import { div } from "framer-motion/client";
+
+export function HeroHighlightDemo() {
+  return (
+    <div>
+
+    <HeroHighlight>
+      <motion.h1
+        initial={{
+          opacity: 0,
+          y: 20,
+        }}
+        animate={{
+          opacity: 1,
+          y: [20, -5, 0],
+        }}
+        transition={{
+          duration: 0.5,
+          ease: [0.4, 0.0, 0.2, 1],
+        }}
+        className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+      >
+       "Chakravyuh" A grand battle awaits{" "}
+        <Highlight className="text-black dark:text-white">
+         Register Now
+        </Highlight>
+      </motion.h1>
+    </HeroHighlight>
+    </div>
+  );
+}
